7435eaae970b5eb545ccd7e1425a5cf738247324
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2021-05-18 17:19:25 +0530

    #parse_mode

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -148,6 +148,7 @@ async def rename_doc(bot, message):
                 chat_id=message.chat.id,
                 document=new_file_name,
                 thumb=thumb_image_path,
+                parse_mode="md",
                 caption=script.CAPTION.format(description),
                 # reply_markup=reply_markup,
                 reply_to_message_id=message.reply_to_message.message_id,
