914351931763b59a4ba57ffc79964cd6f9deac06
Vampire421 <91833744+Vampire421@users.noreply.github.com>
2021-10-05 07:54:27 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -30,7 +30,8 @@ def help_user(bot, update):
     bot.send_message(
         chat_id=update.chat.id,
         text=script.HELP_USER,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ Contact DEV ⭕️", url="https://t.me/prgofficial")]]),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ Support ⭕️", url="https://t.me/Housefull_links")]]),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ Contact Dev ⭕️", url="https://t.me/saminsumesh")]]),
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
